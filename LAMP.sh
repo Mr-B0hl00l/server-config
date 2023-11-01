76111acefff1d46afed5e5da5c23ad8165bd69a0
Mr B0hl00l <127428795+Mr-B0hl00l@users.noreply.github.com>
2023-11-01 05:21:20 -0700

    Update LAMP.sh

--- LAMP.sh
+++ LAMP.sh
@@ -6,5 +6,5 @@ echo -e "[Apache Full]\ntitle=Web Server (HTTP,HTTPS,SSH)\ndescription=Apache we
 ufw allow http
 ufw allow https
 ufw allow "Apache Full"
-apt install php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl libapache2-mod-php8.1 php-mysql php-xml php8.1-gd -y
-apt install mysql-server
+apt install -y php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl libapache2-mod-php8.1 php-mysql php-xml php8.1-gd -y
+apt install -y mysql-server
